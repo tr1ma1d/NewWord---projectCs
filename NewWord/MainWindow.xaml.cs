3682131ba8a4ca3b691747ec0bbaeefb38126afd
Nikita <musatovla3xnikita@gmail.com>
2023-10-12 17:00:47 +0300

    Добавьте файлы проекта.

---
+++ NewWord/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewWord
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+           
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // make a button, which can add dictionary
+       
+        private void AddNewWord()
+        {
+            string inputWord = tbInputWordName.Text;
+            string inputValue = tbInputValue.Text;
+
+            libDic.DictWord(inputWord, inputValue);
+            
+        }
+
+        private void btAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewWord();
+            tbInputValue.Text = null;
+            tbInputWordName.Text = null;
+        }
+
+        private void bdTest_Click(object sender, RoutedEventArgs e)
+        {
+            Test testWindow = new Test();
+            try
+            {
+                this.Hide();
+                testWindow.Show();
+            }
+            catch {
+                MessageBox.Show("404");
+            }
+        }
+
+        private void tbInputWordValue(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ NewWord/libDic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewWord
+{
+    static internal class libDic
+    {
+        static Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        
+    
+        static public void DictWord(string word, string valueWord)
+        {
+            dictionary.Add(word, valueWord);
+        }
+
+    }
+}
